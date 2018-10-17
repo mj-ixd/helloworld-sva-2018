8818e230c6c82a0a153d222ef2c9feb8d90edb3e
mj-ixd <mkim181@sva.edu>
2018-10-17 12:57:18 -0400

    index

---
+++ week4/1003.py
@@ -0,0 +1,22 @@
+import turtle
+
+wn = turtle.Screen()
+wn.bgcolor("black")
+
+mj = turtle.Turtle()
+mj.speed(120)
+mj.color("blue")
+
+
+def drawArt(d,angle):
+    for i in range(1,200):
+      mj.forward(d)
+      mj.left(angle)
+      d = d - 3
+
+drawArt(400,120)
+drawArt(400,120)
+drawArt(600,300)
+drawArt(600,300)
+drawArt(400,120)
+drawArt(600,300)
